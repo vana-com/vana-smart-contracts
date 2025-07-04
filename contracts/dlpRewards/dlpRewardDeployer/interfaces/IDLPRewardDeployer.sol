53ccef9d5df14b06a3706f9fb938c2306e92a39b
dumedco <ec.dumea@gmail.com>
2025-07-04 22:28:40 +0200

    refact dlp reward deployer (#34)
    * refact dlp reward deployer
    
    * fix dlp reward simulate script

--- contracts/dlpRewards/dlpRewardDeployer/interfaces/IDLPRewardDeployer.sol
+++ contracts/dlpRewards/dlpRewardDeployer/interfaces/IDLPRewardDeployer.sol
@@ -25,6 +25,9 @@ interface IDLPRewardDeployer {
 
     struct EpochReward {
         mapping(uint256 dlpId => EpochDlpReward epochDlpReward) epochDlpRewards;
+        uint256 distributionInterval; //number of blocks; usual 1 day ( 3600 * 24 / 6)
+        uint256 numberOfTranches;
+        uint256 remediationWindow; //number of blocks to wait before starting distributing rewards
     }
 
     function version() external pure returns (uint256);
@@ -32,9 +35,16 @@ interface IDLPRewardDeployer {
     function vanaEpoch() external view returns (IVanaEpoch);
     function dlpRewardSwap() external view returns (IDLPRewardSwap);
     function treasury() external view returns (ITreasury);
-    function numberOfTranches() external view returns (uint256);
+    function numberOfBlocksBetweenTranches() external view returns (uint256);
     function rewardPercentage() external view returns (uint256);
     function maximumSlippagePercentage() external view returns (uint256);
+
+    struct EpochRewardInfo {
+        uint256 distributionInterval; //number of blocks; usual 1 day ( 3600 * 24 / 6)
+        uint256 numberOfTranches;
+        uint256 remediationWindow; //number of blocks to wait before starting distributing rewards
+    }
+    function epochRewards(uint256 epochId) external view returns (EpochRewardInfo memory);
 
     struct EpochDlpRewardInfo {
         uint256 totalDistributedAmount;
@@ -55,8 +65,13 @@ interface IDLPRewardDeployer {
     function updateTreasury(address treasuryAddress) external;
     function updateRewardPercentage(uint256 newRewardPercentage) external;
     function updateMaximumSlippagePercentage(uint256 newMaximumSlippagePercentage) external;
-
+    function updateNumberOfBlocksBetweenTranches(uint256 newNumberOfBlocksBetweenTranches) external;
     function distributeRewards(uint256 epochId, uint256[] calldata dlpIds) external;
-
     function withdrawEpochDlpPenaltyAmount(uint256 epochId, uint256 dlpId, address recipientAddress) external;
+    function initializeEpochRewards(
+        uint256 epochId,
+        uint256 distributionInterval,
+        uint256 numberOfTranches,
+        uint256 remediationWindow
+    ) external;
 }
