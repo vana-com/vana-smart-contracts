55b0e200a02310450071130e7afa3dd725716f67
Eduard Dumea <ec.dumea@gmail.com>
2025-10-07 20:54:30 +0300

    multiple reward distribution simulation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,7 +73,7 @@ const config: HardhatUserConfig = {
       chainId: 1480,
       forking: {
         url: process.env.VANA_RPC_URL || "",
-        // blockNumber: 1283121,
+        // blockNumber: 5203720,
         // url: process.env.MOKSHA_RPC_URL || "",
         // blockNumber: 2_569_780,
       },
