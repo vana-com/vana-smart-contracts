3345145639cc6db8f9ed77f2f1724649caee23fc
Volod <volod@vana.com>
2025-10-24 15:05:07 -0700

    feat: calculates the optimal swap amount based on price position and swap direction

--- contracts/buyAndBurn/BuyAndBurnSwapImplementation.sol
+++ contracts/buyAndBurn/BuyAndBurnSwapImplementation.sol
@@ -84,328 +84,49 @@ contract BuyAndBurnSwapImplementation is
         positionManager = newPositionManager;
     }
 
-    function getAmountsDelta(
-        uint160 sqrtRatioX96,
+    /// @notice Calculates the optimal swap amount based on price position and swap direction
+    /// @param amountIn Total amount of input token
+    /// @param currentSqrtPriceX96 Current pool price
+    /// @param sqrtRatioLowerX96 Lower bound of LP position
+    /// @param sqrtRatioUpperX96 Upper bound of LP position
+    /// @param zeroForOne Direction of swap (true = token0→token1, false = token1→token0)
+    /// @return Recommended amount to swap
+    function calculateSwapAmount(
+        uint256 amountIn,
+        uint160 currentSqrtPriceX96,
         uint160 sqrtRatioLowerX96,
         uint160 sqrtRatioUpperX96,
-        uint128 liquidity
-    ) internal pure returns (uint256 amount0, uint256 amount1) {
-        require(sqrtRatioLowerX96 <= sqrtRatioUpperX96, BuyAndBurnSwap__InvalidRange());
-
-        if (sqrtRatioX96 <= sqrtRatioLowerX96) {
-            amount0 = SqrtPriceMath.getAmount0Delta(sqrtRatioLowerX96, sqrtRatioUpperX96, liquidity, true);
-        } else if (sqrtRatioX96 < sqrtRatioUpperX96) {
-            amount0 = SqrtPriceMath.getAmount0Delta(sqrtRatioX96, sqrtRatioUpperX96, liquidity, true);
-            amount1 = SqrtPriceMath.getAmount1Delta(sqrtRatioLowerX96, sqrtRatioX96, liquidity, true);
-        } else {
-            amount1 = SqrtPriceMath.getAmount1Delta(sqrtRatioLowerX96, sqrtRatioUpperX96, liquidity, true);
-        }
-    }
-
-    struct QuoteLpSwapParams {
-        uint256 amountIn;
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        uint256 maximumSlippagePercentage;
-        uint160 sqrtRatioLowerX96;
-        uint160 sqrtRatioUpperX96;
-    }
-
-    struct LpSwapQuote {
-        uint256 amountSwapIn;
-        uint256 spareIn;
-        uint256 spareOut;
-        uint128 liquidityDelta;
-        uint160 sqrtPriceX96After;
-    }
-
-    function quoteLpSwap(QuoteLpSwapParams memory params) internal view returns (LpSwapQuote memory res) {
-        require(params.amountIn > 0, BuyAndBurnSwap__ZeroAmount());
-        require(params.sqrtRatioLowerX96 <= params.sqrtRatioUpperX96, BuyAndBurnSwap__InvalidRange());
-
-        IWVANA WVANA = swapHelper.WVANA();
-        address tokenIn = params.tokenIn == VANA ? address(WVANA) : params.tokenIn;
-        address tokenOut = params.tokenOut == VANA ? address(WVANA) : params.tokenOut;
-
-        IUniswapV3Pool pool = swapHelper.getPool(tokenIn, tokenOut, params.fee);
-        (uint160 currentSqrtPriceX96, , , , , , ) = pool.slot0();
-
-        uint128 currentLiquidity = pool.liquidity();
-
-        bool zeroForOne = tokenIn < tokenOut;
-
-        uint128 bestLiquidityDelta = 0;
-        uint256 bestAmount0Used = 0;
-        uint256 bestAmount1Used = 0;
-
-        /// @dev Cases of increaseLiquidity without swap, so the price does not move
-        if (currentSqrtPriceX96 <= params.sqrtRatioLowerX96 && zeroForOne) {
-            /// @dev The current sqrtPriceX96 is below the specified range.
-            /// The position is only activated when the price goes up
-            /// (aka only when the pool sells token0).
-            /// Thus, only tokenIn (token0) is provided and no need to swap.
-            bestLiquidityDelta = LiquidityAmounts.getLiquidityForAmount0(
-                params.sqrtRatioLowerX96,
-                params.sqrtRatioUpperX96,
-                params.amountIn
-            );
-            bestAmount0Used = SqrtPriceMath.getAmount0Delta(
-                params.sqrtRatioLowerX96,
-                params.sqrtRatioUpperX96,
-                bestLiquidityDelta,
-                true
-            );
-            return
-                LpSwapQuote({
-                    amountSwapIn: 0,
-                    spareIn: params.amountIn - bestAmount0Used,
-                    spareOut: 0,
-                    liquidityDelta: bestLiquidityDelta,
-                    sqrtPriceX96After: currentSqrtPriceX96
-                });
+        bool zeroForOne
+    ) internal pure returns (uint256) {
+        // No swap needed if price is outside range in the "right" direction
+        if ((currentSqrtPriceX96 <= sqrtRatioLowerX96 && zeroForOne) ||
+            (currentSqrtPriceX96 >= sqrtRatioUpperX96 && !zeroForOne)) {
+            return 0;
         }
 
-        if (currentSqrtPriceX96 >= params.sqrtRatioUpperX96 && !zeroForOne) {
-            /// @dev The current sqrtPriceX96 is above the specified range.
-            /// The position is only activated when the price goes down
-            /// (aka only when the pool sells token1).
-            /// Thus, only tokenIn (token1) is provided and no need to swap.
-            bestLiquidityDelta = LiquidityAmounts.getLiquidityForAmount1(
-                params.sqrtRatioLowerX96,
-                params.sqrtRatioUpperX96,
-                params.amountIn
-            );
-            bestAmount1Used = SqrtPriceMath.getAmount1Delta(
-                params.sqrtRatioLowerX96,
-                params.sqrtRatioUpperX96,
-                bestLiquidityDelta,
-                true
-            );
-            return
-                LpSwapQuote({
-                    amountSwapIn: 0,
-                    spareIn: params.amountIn - bestAmount1Used,
-                    spareOut: 0,
-                    liquidityDelta: bestLiquidityDelta,
-                    sqrtPriceX96After: currentSqrtPriceX96
-                });
-        }
-
-        /// @dev Cases of increaseLiquidity with swap from tokenIn to tokenOut.
-        /// The price moves down if tokenIn is token0, up if tokenIn is token1.
-
-        uint256 low = 1;
-        uint256 high = params.amountIn;
-        uint256 bestSwapIn = 0;
-        uint256 bestSwapOut = 0;
-        uint160 bestSqrtPriceX96After = 0;
-
-        while (low < high) {
-            uint256 mid = low + (high - low) / 2;
-
-            ISwapHelper.Quote memory quote = swapHelper.quoteSlippageExactInputSingle(
-                ISwapHelper.QuoteSlippageExactInputSingleParams({
-                    tokenIn: tokenIn,
-                    tokenOut: tokenOut,
-                    fee: params.fee,
-                    amountIn: mid,
-                    sqrtPriceX96: currentSqrtPriceX96,
-                    liquidity: currentLiquidity,
-                    maximumSlippagePercentage: params.maximumSlippagePercentage
-                })
-            );
-
-            /// @dev Amounts to be added to the position after the swap.
-            uint256 amount0 = zeroForOne ? params.amountIn - quote.amountToPay : quote.amountReceived;
-            uint256 amount1 = zeroForOne ? quote.amountReceived : params.amountIn - quote.amountToPay;
-
-            uint128 liquidityDelta;
-
-            if (quote.sqrtPriceX96After <= params.sqrtRatioLowerX96) {
-                liquidityDelta = LiquidityAmounts.getLiquidityForAmount0(
-                    params.sqrtRatioLowerX96,
-                    params.sqrtRatioUpperX96,
-                    amount0
-                );
-
-                if (zeroForOne) {
-                    /// @dev All remaining tokenIn (token0) after the swap is added to the position,
-                    /// while the tokenOut (token1) is not added to the pool. We should use less token0 in the swap,
-                    /// so that more token0 can be added to the position.
-                    /// zeroForOne purchase: sqrtPriceLimitX96 <= sqrtPriceX96After.
-                    /// When using less token0 in the swap, the price goes up so the limit is not reached.
-                    high = mid;
-                } else {
-                    /// @dev Only tokenOut (token0) is added to the position.
-                    /// We should use more tokenIn (token1) in the swap to get more token0.
-                    /// oneForZero purchase: sqrtPriceX96After <= sqrtPriceLimitX96.
-                    /// When using more token1 in the swap, the price goes up to the limit.
-                    /// If we reach the limit, when putting more token1, we still get the same amount of token0.
-                    if (quote.sqrtPriceX96After == quote.sqrtPriceLimitX96) {
-                        if (liquidityDelta > bestLiquidityDelta) {
-                            bestLiquidityDelta = liquidityDelta;
-                            bestSwapIn = quote.amountToPay;
-                            bestSwapOut = quote.amountReceived;
-                            bestSqrtPriceX96After = quote.sqrtPriceX96After;
-                        }
-                        break;
-                    }
-                    low = mid + 1;
-                }
-            } else if (quote.sqrtPriceX96After < params.sqrtRatioUpperX96) {
-                uint128 liquidity0 = LiquidityAmounts.getLiquidityForAmount0(
-                    quote.sqrtPriceX96After,
-                    params.sqrtRatioUpperX96,
-                    amount0
-                );
-                uint128 liquidity1 = LiquidityAmounts.getLiquidityForAmount1(
-                    params.sqrtRatioLowerX96,
-                    quote.sqrtPriceX96After,
-                    amount1
-                );
-                if (liquidity0 < liquidity1) {
-                    liquidityDelta = liquidity0;
-
-                    /// @dev token0 is the limiting factor. We should have more token0
-                    /// after the swap to increase the liquidity more.
-                    if (zeroForOne) {
-                        /// @dev We should use less token0 in the swap.
-                        high = mid;
-                    } else {
-                        /// @dev token0 is tokenOut.
-                        if (quote.sqrtPriceX96After == quote.sqrtPriceLimitX96) {
-                            if (liquidityDelta > bestLiquidityDelta) {
-                                bestLiquidityDelta = liquidityDelta;
-                                bestSwapIn = quote.amountToPay;
-                                bestSwapOut = quote.amountReceived;
-                                bestSqrtPriceX96After = quote.sqrtPriceX96After;
-                            }
-                            break;
-                        }
-                        /// @dev We should use more token1 in the swap to have more token0.
-                        low = mid + 1;
-                    }
-                } else if (liquidity0 > liquidity1) {
-                    liquidityDelta = liquidity1;
-
-                    /// @dev token1 is the limiting factor. We should have more token1
-                    /// after the swap to increase the liquidity more.
-                    if (zeroForOne) {
-                        /// @dev token1 is tokenOut.
-                        if (quote.sqrtPriceX96After == quote.sqrtPriceLimitX96) {
-                            if (liquidityDelta > bestLiquidityDelta) {
-                                bestLiquidityDelta = liquidityDelta;
-                                bestSwapIn = quote.amountToPay;
-                                bestSwapOut = quote.amountReceived;
-                                bestSqrtPriceX96After = quote.sqrtPriceX96After;
-                            }
-                            break;
-                        }
-                        /// @dev We should use more token0 in the swap to have more token1.
-                        low = mid + 1;
-                    } else {
-                        /// @dev We should use less token1 in the swap.
-                        high = mid;
-                    }
-                } else {
-                    // liquidity0 == liquidity1
-                    /// @dev The swap is optimal.
-                    liquidityDelta = liquidity0;
-
-                    if (liquidityDelta > bestLiquidityDelta) {
-                        bestLiquidityDelta = liquidityDelta;
-                        bestSwapIn = quote.amountToPay;
-                        bestSwapOut = quote.amountReceived;
-                        bestSqrtPriceX96After = quote.sqrtPriceX96After;
-                    }
-                    break;
-                }
+        // Determine optimal swap percentage based on price position and direction
+        if (currentSqrtPriceX96 < sqrtRatioLowerX96) {
+            // Price below range → need more token0
+            if (zeroForOne) {
+                // Swapping token0→token1: keep most token0, swap 25%
+                return amountIn / 4;
             } else {
-                // quote.sqrtPriceX96After >= params.sqrtRatioUpperX96
-                liquidityDelta = LiquidityAmounts.getLiquidityForAmount1(
-                    params.sqrtRatioLowerX96,
-                    params.sqrtRatioUpperX96,
-                    amount1
-                );
-
-                if (zeroForOne) {
-                    /// @dev Only tokenOut (token1) is added to the position.
-                    /// We should use more tokenIn (token0) in the swap to get more tokenOut.
-                    /// zeroForOne purchase: sqrtPriceLimitX96 <= sqrtPriceX96After.
-                    /// When using more token0 in the swap, the price goes down to the limit.
-                    /// If we reach the limit, when putting more token0, we still get the same amount of token1.
-                    if (quote.sqrtPriceX96After == quote.sqrtPriceLimitX96) {
-                        if (liquidityDelta > bestLiquidityDelta) {
-                            bestLiquidityDelta = liquidityDelta;
-                            bestSwapIn = quote.amountToPay;
-                            bestSwapOut = quote.amountReceived;
-                            bestSqrtPriceX96After = quote.sqrtPriceX96After;
-                        }
-                        break;
-                    }
-                    low = mid + 1;
-                } else {
-                    /// @dev All remaining tokenIn (token1) after the swap is added to the position,
-                    /// while the tokenOut is not added to the pool. We should use less token1 in the swap,
-                    /// so that more token1 can be added to the position.
-                    /// oneForZero purchase: sqrtPriceX96After <= sqrtPriceLimitX96.
-                    /// When using less token1 in the swap, the price goes down so the limit is not reached.
-                    high = mid;
-                }
+                // Swapping token1→token0: need to acquire token0, swap 75%
+                return (amountIn * 3) / 4;
             }
-
-            if (liquidityDelta > bestLiquidityDelta) {
-                bestLiquidityDelta = liquidityDelta;
-                bestSwapIn = quote.amountToPay;
-                bestSwapOut = quote.amountReceived;
-                bestSqrtPriceX96After = quote.sqrtPriceX96After;
+        } else if (currentSqrtPriceX96 > sqrtRatioUpperX96) {
+            // Price above range → need more token1
+            if (zeroForOne) {
+                // Swapping token0→token1: need to acquire token1, swap 75%
+                return (amountIn * 3) / 4;
+            } else {
+                // Swapping token1→token0: keep most token1, swap 25%
+                return amountIn / 4;
             }
+        } else {
+            // Price in range → need both tokens relatively balanced, swap 50%
+            return amountIn / 2;
         }
-
-        if (low == high) {
-            ISwapHelper.Quote memory quote = swapHelper.quoteSlippageExactInputSingle(
-                ISwapHelper.QuoteSlippageExactInputSingleParams({
-                    tokenIn: tokenIn,
-                    tokenOut: tokenOut,
-                    fee: params.fee,
-                    amountIn: low,
-                    sqrtPriceX96: currentSqrtPriceX96,
-                    liquidity: currentLiquidity,
-                    maximumSlippagePercentage: params.maximumSlippagePercentage
-                })
-            );
-            uint256 amount0 = zeroForOne ? params.amountIn - quote.amountToPay : quote.amountReceived;
-            uint256 amount1 = zeroForOne ? quote.amountReceived : params.amountIn - quote.amountToPay;
-            uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
-                quote.sqrtPriceX96After,
-                params.sqrtRatioLowerX96,
-                params.sqrtRatioUpperX96,
-                amount0,
-                amount1
-            );
-            if (liquidity > bestLiquidityDelta) {
-                bestLiquidityDelta = liquidity;
-                bestSwapIn = quote.amountToPay;
-                bestSwapOut = quote.amountReceived;
-                bestSqrtPriceX96After = quote.sqrtPriceX96After;
-            }
-        }
-
-        (bestAmount0Used, bestAmount1Used) = getAmountsDelta(
-            bestSqrtPriceX96After,
-            params.sqrtRatioLowerX96,
-            params.sqrtRatioUpperX96,
-            bestLiquidityDelta
-        );
-
-        res.liquidityDelta = bestLiquidityDelta;
-        res.sqrtPriceX96After = bestSqrtPriceX96After;
-        res.amountSwapIn = bestSwapIn;
-        res.spareIn = zeroForOne
-            ? params.amountIn - bestSwapIn - bestAmount0Used
-            : params.amountIn - bestSwapIn - bestAmount1Used;
-        res.spareOut = zeroForOne ? bestSwapOut - bestAmount1Used : bestSwapOut - bestAmount0Used;
     }
 
     function swapAndAddLiquidity(
